e6860a8161a2c766c2c016ccee763369f11a2587
Srishty632 <srishtyvidyarthi@gmail.com>
2026-05-30 21:16:36 +0530

    fix prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,16 @@
-import { PrismaClient } from "@prisma/client/edge";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const connectionString = process.env.DATABASE_URL!;
-const adapter = new PrismaPg({ connectionString });
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({ adapter } as any);
+  global.prisma ??
+  new PrismaClient({
+    adapter: new PrismaPg({ connectionString }) as any,
+  } as any);
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
